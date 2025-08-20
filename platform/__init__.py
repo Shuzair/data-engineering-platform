8ae17453c5e8deafdf20e88e9546a35389c84445
Shahmir Bin Asif <shahmirasif.j97@gmail.com>
2025-08-20 09:51:07 +0700

    Create __init__.py

---
+++ platform/__init__.py
@@ -0,0 +1,11 @@
+"""
+Data Engineering Platform v2.0
+A modular, containerized data engineering platform.
+"""
+
+__version__ = "2.0.0"
+__author__ = "Your Name"
+
+from platform.core import Platform
+
+__all__ = ["Platform"]
